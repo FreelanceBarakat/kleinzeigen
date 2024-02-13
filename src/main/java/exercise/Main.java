40c1b990f5b9dbbf19564c08c213562394cf4c11
moustafa.barakat <m.g.y@live.com>
2024-02-13 15:44:41 +0100

    initial code challenge as is

---
+++ src/main/java/exercise/Main.java
@@ -0,0 +1,13 @@
+package exercise;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Number of females inside address book: TODO");
+
+        System.out.println("Oldest person inside address book: TODO");
+    }
+
+}
